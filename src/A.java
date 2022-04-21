f000d9d9a0d5031c3ed739908cb988e537d98a8f
chinu <pattnayakchinu143@gmail.com>
2022-04-21 16:54:48 +0530

    trial code

---
+++ src/A.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class A {
+public static void main(String[] args) {
+	FirefoxDriver d= new FirefoxDriver();
+//	d.get("http:///www.gmail.com");
+}
+}
